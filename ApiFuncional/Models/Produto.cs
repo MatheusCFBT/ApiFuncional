a12bebb563c5033a4d00e4b48bf14c72964e1b55
MatheusCFBT <matheusterzini@gmail.com>
2024-07-31 11:01:22 -0300

    Add project files.

---
+++ ApiFuncional/Models/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFuncional.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(60, MinimumLength = 2, ErrorMessage = "O campo {0} deve conter entre {2} e {1} caracteres.")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
+        public decimal Preco { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public int QuantidadeEstoque { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(120, MinimumLength = 2, ErrorMessage = "O campo {0} deve conter entre {2} e {1} caracteres.")]
+        public string? Descricao { get; set; }
+    }
+}
