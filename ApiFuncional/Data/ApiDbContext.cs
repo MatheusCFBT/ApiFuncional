976b6401f621d4ee649042240ac5996bb2f4de51
MatheusCFBT <matheusterzini@gmail.com>
2024-08-02 11:18:09 -0300

    Migration adicionada para a model Produtos.

--- ApiFuncional/Data/ApiDbContext.cs
+++ ApiFuncional/Data/ApiDbContext.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ApiFuncional.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiFuncional.Data
 {
     public class ApiDbContext : DbContext
     {
-        
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
     }
+
 }
