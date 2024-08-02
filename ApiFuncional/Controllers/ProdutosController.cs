2801958f46ba53739b2b7aa94cde43fd8db3bf0f
MatheusCFBT <matheusterzini@gmail.com>
2024-08-02 11:51:45 -0300

    Controller da API funcionando com operações CRUD

---
+++ ApiFuncional/Controllers/ProdutosController.cs
@@ -0,0 +1,62 @@
+﻿using ApiFuncional.Data;
+using ApiFuncional.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiFuncional.Controllers
+{
+    [ApiController]
+    [Route("api/produtos")]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ApiDbContext _dbContext;
+
+        public ProdutosController(ApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos()
+        {
+            return await _dbContext.Produtos.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Produto>> GetProduto(int id)
+        {
+            var produto = await _dbContext.Produtos.FindAsync(id);
+
+            return produto; 
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Produto>> PostProduto(Produto produto)
+        {
+            _dbContext.Produtos.Add(produto);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProduto), new { id = produto.Id }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> PutProduto(int id, Produto produto)
+        {
+            _dbContext.Update(produto);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteProduto(int id)
+        {
+            var produto = await _dbContext.Produtos.FindAsync(id);
+
+            _dbContext.Produtos.Remove(produto);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
