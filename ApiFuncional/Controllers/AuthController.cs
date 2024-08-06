daf3d256bfc2655ddd33a89eedae586c2d0bdbbc
MatheusCFBT <matheusterzini@gmail.com>
2024-08-06 16:31:50 -0300

    Adicionado a controller de authenticação de usuário com JWT

---
+++ ApiFuncional/Controllers/AuthController.cs
@@ -0,0 +1,90 @@
+﻿using ApiFuncional.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ApiFuncional.Controllers
+{
+    [ApiController]
+    [Route("api/conta")]
+    public class AuthController: ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly JwtSettings _jwtSettings;
+
+        public AuthController(SignInManager<IdentityUser> singInManager,
+                              UserManager<IdentityUser> userManager,
+                              IOptions<JwtSettings> jwtSettings)
+        {
+            _signInManager = singInManager;
+            _userManager = userManager;
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        [HttpPost("registrar")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Registrar(RegisterUserViewModel registerUser)
+        {
+            if (ModelState.IsValid) return ValidationProblem(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+
+            if(result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Ok(GerarJwt());
+            }
+
+            return Problem("Falha ao registrar usuário.");
+        }
+
+        [HttpPost("login")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Login(LoginUserViewModel loginUser)
+        {
+            if (ModelState.IsValid) return ValidationProblem(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+
+            if (result.Succeeded)
+            {
+                return Ok(GerarJwt());
+            }
+
+            return Problem("Usuário ou senha incorretos.");
+        }
+
+        private string GerarJwt()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.Segredo);
+
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _jwtSettings.Emissor,
+                Audience = _jwtSettings.Audiencia,
+                Expires = DateTime.UtcNow.AddHours(_jwtSettings.ExpiracaoHoras),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            var encodedToken = tokenHandler.WriteToken(token);
+
+            return encodedToken;
+        }
+    }
+}
