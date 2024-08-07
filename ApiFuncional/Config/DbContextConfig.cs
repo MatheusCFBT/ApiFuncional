2e4a42ee84ce90ca8ffc1d8d85ecbd5a2e43471a
MatheusCFBT <matheusterzini@gmail.com>
2024-08-07 13:08:27 -0300

    Foi organizado o arquivo Program

---
+++ ApiFuncional/Config/DbContextConfig.cs
@@ -0,0 +1,19 @@
+﻿using ApiFuncional.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace ApiFuncional.Config
+{
+    public static class DbContextConfig
+    {
+        public static WebApplicationBuilder AddDbContextConfig(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<ApiDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return builder;
+        }
+    }
+}
